4fe694ff3f20e4afff15f7c1c03784db2e033be0
Jamil <jamilgeor@gmail.com>
2017-07-02 22:38:29 +1200

    Added Lesson 2 code

---
+++ Lesson02/FormsTutor/ArticlesPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace FormsTutor
+{
+    public partial class ArticlesPage : ContentPage
+    {
+        public ArticlesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
