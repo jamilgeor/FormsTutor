17f3ec7f66492b3292703889cec08ec9d54cadb3
Jamil <jamilgeor@gmail.com>
2017-07-12 14:26:47 +1200

    Added lesson 3 code.

---
+++ Lesson03/FormsTutor/ArticlesPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Reactive.Disposables;
+using FormsTutor.ViewModels;
+using ReactiveUI;
+using Xamarin.Forms;
+
+namespace FormsTutor
+{
+    public partial class ArticlesPage : ContentPage, IViewFor<ArticleListViewModel>
+    {
+        readonly CompositeDisposable _bindingsDisposable = new CompositeDisposable();
+
+        public ArticlesPage()
+        {
+            InitializeComponent();
+        }
+
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+			ViewModel = new ArticleListViewModel();
+
+            this.OneWayBind(ViewModel, vm => vm.Articles, v => v.Articles.ItemsSource).DisposeWith(_bindingsDisposable);
+		}
+
+		protected override void OnDisappearing()
+		{
+			base.OnDisappearing();
+			_bindingsDisposable.Clear();
+		}
+
+		#region ViewModel Setup
+		public ArticleListViewModel ViewModel { get; set; }
+		object IViewFor.ViewModel
+		{
+			get { return ViewModel; }
+			set { ViewModel = (ArticleListViewModel)value; }
+		}
+		#endregion
+	}
+}
---
+++ Lesson03/FormsTutor/ViewModels/ArticlesViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ReactiveUI;
+
+namespace FormsTutor.ViewModels
+{
+    public class ArticleListViewModel : ReactiveObject
+    {
+        ReactiveList<string> _articles;
+		public ReactiveList<string> Articles
+		{
+			get { return _articles; }
+			set { this.RaiseAndSetIfChanged(ref _articles, value); }
+		}
+
+		public ArticleListViewModel()
+		{
+			Articles = new ReactiveList<string> { "Article 1", "Article 2", "Article 3", "Article 4" };
+		}
+    }
+}
