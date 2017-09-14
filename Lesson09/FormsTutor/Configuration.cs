f0a45253261cb91c46a9509f3f136970f2101268
Jamil <jamilgeor@gmail.com>
2017-09-14 15:44:20 +1200

    Updated data to include latest lesson.

--- Lesson09/FormsTutor/Configuration.cs
+++ Lesson09/FormsTutor/Configuration.cs
@@ -5,7 +5,7 @@ namespace FormsTutor
     public static class Configuration
     {
         public const string ApplicationName = "FormsTutor";
-        public const string ApiBaseUrl = "https://raw.githubusercontent.com/jamilgeor/FormsTutor/master/Lesson08/Data/";
+        public const string ApiBaseUrl = "https://raw.githubusercontent.com/jamilgeor/FormsTutor/master/Lesson09/Data/";
         public const string BlogBaseUrl = "https://jamilgeor.com";
         public const int NumberOfWebRequestRetries = 3;
     }
